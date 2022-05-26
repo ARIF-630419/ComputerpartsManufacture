b9fe00f9ed654027247c602a5d11bbf89cfe6241
Md. Ariful Islam <ariful25-013@diu.edu.bd>
2022-05-27 00:21:42 +0600

    update portfolio

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -4,7 +4,7 @@ const Portfolio = () => {
     return (
         <div className='mx-auto mt-10 p-4 w-2/4 '>
             <h2 className='text-2xl pb-2'>Name: Md. Ariful Islam</h2>
-            <h2 className='text-xl pb-2'>Email: ariful@diu.edu.bd</h2>
+            <h2 className='text-xl pb-2'>Email: ariful25-013@diu.edu.bd</h2>
             <h2 className='text-xl pb-2'>Educational Background: MSc in CSE</h2>
             <h2 className='text-xl pb-2'>List of Technologies: HTML,CSS,JavaScript,React,Node js,Mongodb etc.</h2>
             <h2 className='text-xl pb-2'>Top 3 website link.</h2>
