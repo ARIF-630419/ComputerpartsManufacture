b9ecd7c459f8af84cc9dc9d60304dfe171248dfe
Md. Ariful Islam <ariful25-013@diu.edu.bd>
2022-05-27 00:15:28 +0600

    portfolio page added

--- src/Pages/Portfolio/Portfolio.js
+++ src/Pages/Portfolio/Portfolio.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Portfolio = () => {
     return (
-        <div>
-            <h2>This is portfolio</h2>
+        <div className='mx-auto mt-10 p-4 w-2/4 '>
+            <h2 className='text-2xl pb-2'>Name: Md. Ariful Islam</h2>
+            <h2 className='text-xl pb-2'>Email: ariful@diu.edu.bd</h2>
+            <h2 className='text-xl pb-2'>Educational Background: MSc in CSE</h2>
+            <h2 className='text-xl pb-2'>List of Technologies: HTML,CSS,JavaScript,React,Node js,Mongodb etc.</h2>
+            <h2 className='text-xl pb-2'>Top 3 website link.</h2>
+            <h2 className='text-xl pb-2'>1. https://computer-warehouse-client.web.app/ </h2>
+            <h2 className='text-xl pb-2'>2. https://doctor-service-provider-3bfa2.web.app/</h2>
+            <h2 className='text-xl pb-2'>3. https://clinquant-speculoos-3b31b7.netlify.app/</h2>
         </div>
     );
 };
