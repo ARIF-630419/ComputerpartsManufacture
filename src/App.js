1bb7ac5f18ceab9abd91bc1c0cbafddcad39da30
Md. Ariful Islam <ariful25-013@diu.edu.bd>
2022-05-28 01:19:45 +0600

    login and signup page add

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Home from './Pages/Home/Home';
 import Portfolio from './Pages/Portfolio/Portfolio';
 import Purchases from './Pages/Purchase/Purchases';
 import Navbar from './Pages/Shared/Navbar';
+import Login from './Pages/Login/Login'
+import SignUp from './Pages/Login/SignUp'
+import RequireAuth from './Pages/Login/RequireAuth';
 function App() {
   return (
     <div>
@@ -13,8 +16,13 @@ function App() {
         <Route path="/" element={<Home></Home>} />
         <Route path="blogs" element={<Blogs></Blogs>} />
         <Route path="portfolio" element={<Portfolio></Portfolio>} />
-        <Route path="purchase" element={<Purchases></Purchases>} />
-
+        <Route path="purchase" element={
+          <RequireAuth>
+            <Purchases></Purchases>
+          </RequireAuth>
+        } />
+        <Route path="login" element={<Login></Login>} />
+        <Route path="signup" element={<SignUp></SignUp>} />
       </Routes>
     </div>
   );
