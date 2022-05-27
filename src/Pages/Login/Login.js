955de39a49c5213ef2918cd0bdb7e2d6f1954677
Md. Ariful Islam <ariful25-013@diu.edu.bd>
2022-05-28 01:34:28 +0600

    update

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -20,7 +20,7 @@ const Login = () => {
     const location = useLocation();
     let from = location.state?.from?.pathname || "/";
 
-    useEffect( () =>{
+    useEffect(() => {
         if (user || gUser) {
             navigate(from, { replace: true });
         }
@@ -30,8 +30,8 @@ const Login = () => {
         return <Loading></Loading>
     }
 
-    if(error || gError){
-        signInError= <p className='text-red-500'><small>{error?.message || gError?.message }</small></p>
+    if (error || gError) {
+        signInError = <p className='text-red-500'><small>{error?.message || gError?.message}</small></p>
     }
 
     const onSubmit = data => {
@@ -97,7 +97,7 @@ const Login = () => {
                         {signInError}
                         <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
                     </form>
-                    <p><small>New to Doctors Portal <Link className='text-primary' to="/signup">Create New Account</Link></small></p>
+                    <p><small>New to Computer Parts Manufucture <Link className='text-primary' to="/signup">Create New Account</Link></small></p>
                     <div className="divider">OR</div>
                     <button
                         onClick={() => signInWithGoogle()}
