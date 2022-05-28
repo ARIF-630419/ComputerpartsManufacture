846ba5743b6786197dfdbb3ad3bdf110d86ccbb8
Md. Ariful Islam <ariful25-013@diu.edu.bd>
2022-05-29 01:50:46 +0600

    manage all order add

--- src/Pages/Dashboard/ManageAllOrders.js
+++ src/Pages/Dashboard/ManageAllOrders.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import ManageAllOrder from './ManageAllOrder';
 
 const ManageAllOrders = () => {
+    const [orders, setOrders] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/allorder')
+            .then(res => res.json())
+            .then(data => setOrders(data));
+    }, [])
     return (
         <div>
-            <h2>Manage All orders</h2>
+            <h2 className='text-center text-4xl text-lime-500 pt-12 pb-5'>Manage All orders</h2>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                {
+                    orders.map(order => <ManageAllOrder
+                        key={order._id}
+                        order={order}
+                    ></ManageAllOrder>)
+                }
+            </div>
         </div>
     );
 };
